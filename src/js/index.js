b260062e244c177f7a41b11b0369608dc154fb08
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-02-04 12:49:37 +0000

    Implement mobile version

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuOpenBtn = document.getElementById('mobile-menu-open');
+    const mobileMenuCloseBtn = document.getElementById('mobile-menu-close');
+    const menu = document.getElementById('menu');
+
+    // Toggle menu
+    [mobileMenuOpenBtn, mobileMenuCloseBtn].forEach(trigger => {
+        trigger.addEventListener('click', () => {
+            menu.classList.toggle('-translate-x-full');
+
+            if (document.body.style.overflow === '') {
+                document.body.style.overflow = 'hidden';
+            } else {
+                document.body.style.overflow = '';
+            }
+        });
+    });
+});
